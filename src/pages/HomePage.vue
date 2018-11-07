6a6668f6c6bbc670b3b1918a73d64179980eff0e
Nathan Shane <nathan.shane@flocabulary.com>
2018-11-06 20:06:35 -0500

    Add text on hover

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,18 @@
 <template>
 <div>
   <router-link to="/all-da-poke">
-    <img alt="Vue logo" src="https://seeklogo.com/images/P/pokeball-logo-DC23868CA1-seeklogo.com.png">
+    <img alt="Vue logo" src="https://seeklogo.com/images/P/pokeball-logo-DC23868CA1-seeklogo.com.png" @mouseover="showText = true" @mouseleave="showText = false">
+    <h1 v-show="showText">Enter the PokeVue!</h1>
   </router-link>
 </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    showText: false
+  })
+};
 </script>
 
 <style scoped>
@@ -18,11 +23,12 @@ div {
   height: 100vh;
 }
 img {
-  margin-bottom: 100px;
-  -webkit-animation: spin 4s linear infinite;
-  -moz-animation: spin 4s linear infinite;
-  animation: spin 4s linear infinite;
+  /* margin-bottom: 300px; */
+  /* -webkit-animation: spin 6s linear infinite;
+  -moz-animation: spin 6s linear infinite;
+  animation: spin 6s linear infinite; */
 }
+
 @-moz-keyframes spin {
   100% {
     -moz-transform: rotate(360deg);
