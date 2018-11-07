cf4337da267a80240933c1eee0cd3adee3407188
Nathan Shane <nathan.shane@flocabulary.com>
2018-11-06 19:53:01 -0500

    Add rotation animation to pokeball

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,8 @@
 <template>
 <div>
-  <img alt="Vue logo" src="https://seeklogo.com/images/P/pokeball-logo-DC23868CA1-seeklogo.com.png">
+  <router-link to="/all-da-poke">
+    <img alt="Vue logo" src="https://seeklogo.com/images/P/pokeball-logo-DC23868CA1-seeklogo.com.png">
+  </router-link>
 </div>
 </template>
 
@@ -16,6 +18,25 @@ div {
   height: 100vh;
 }
 img {
-  padding-bottom: 100px;
+  margin-bottom: 100px;
+  -webkit-animation: spin 4s linear infinite;
+  -moz-animation: spin 4s linear infinite;
+  animation: spin 4s linear infinite;
+}
+@-moz-keyframes spin {
+  100% {
+    -moz-transform: rotate(360deg);
+  }
+}
+@-webkit-keyframes spin {
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
+}
+@keyframes spin {
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
 }
 </style>
