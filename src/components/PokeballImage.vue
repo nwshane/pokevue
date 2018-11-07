59afb7e26b862d516d2d40799b87b31268f79c7a
Nathan Shane <nathan.shane@flocabulary.com>
2018-11-07 08:29:58 -0500

    Create separate PokeballImage component

---
+++ src/components/PokeballImage.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="Vue logo" src="https://seeklogo.com/images/P/pokeball-logo-DC23868CA1-seeklogo.com.png" @mouseover="showText = true" @mouseleave="showText = false">
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+img {
+  -webkit-animation: spin 6s linear infinite;
+  -moz-animation: spin 6s linear infinite;
+  animation: spin 6s linear infinite;
+}
+
+@-moz-keyframes spin {
+  100% {
+    -moz-transform: rotate(360deg);
+  }
+}
+@-webkit-keyframes spin {
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
+}
+@keyframes spin {
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,14 +1,17 @@
 <template>
 <div>
   <router-link to="/all-da-poke">
-    <img alt="Vue logo" src="https://seeklogo.com/images/P/pokeball-logo-DC23868CA1-seeklogo.com.png" @mouseover="showText = true" @mouseleave="showText = false">
+    <PokeballImage />
     <h1 v-show="showText">Enter the PokeVue!</h1>
   </router-link>
 </div>
 </template>
 
 <script>
+import PokeballImage from "../components/PokeballImage";
+
 export default {
+  components: { PokeballImage },
   data: () => ({
     showText: false
   })
@@ -21,28 +24,5 @@ div {
   justify-content: center;
   align-items: center;
   height: 100vh;
-}
-img {
-  /* margin-bottom: 300px; */
-  /* -webkit-animation: spin 6s linear infinite;
-  -moz-animation: spin 6s linear infinite;
-  animation: spin 6s linear infinite; */
-}
-
-@-moz-keyframes spin {
-  100% {
-    -moz-transform: rotate(360deg);
-  }
-}
-@-webkit-keyframes spin {
-  100% {
-    -webkit-transform: rotate(360deg);
-  }
-}
-@keyframes spin {
-  100% {
-    -webkit-transform: rotate(360deg);
-    transform: rotate(360deg);
-  }
 }
 </style>
