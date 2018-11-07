48a6c6f1232edb9c82dca33232a9113322bb8bc0
Nathan Shane <nathan.shane@flocabulary.com>
2018-11-06 19:45:15 -0500

    Center pokeball

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,10 +1,21 @@
 <template>
+<div>
   <img alt="Vue logo" src="https://seeklogo.com/images/P/pokeball-logo-DC23868CA1-seeklogo.com.png">
+</div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style scoped>
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+img {
+  padding-bottom: 100px;
+}
 </style>
